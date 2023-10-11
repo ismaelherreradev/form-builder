3e013678f8a7a5a7949c05fb1ee500d092b08e73
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-10-11 15:15:48 -0400

    dnd overlay

--- components/form-builder/designer/designer.tsx
+++ components/form-builder/designer/designer.tsx
@@ -1,5 +1,7 @@
 import { useDroppable } from "@dnd-kit/core"
 
+import { cn } from "@/lib/utils"
+
 import DesignerSidebar from "./sidebar"
 
 export default function Designer() {
@@ -13,10 +15,22 @@ export default function Designer() {
   return (
     <div className="flex w-full h-full">
       <div className="p-4 w-full">
-        <div className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto">
-          <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
-            Drop here
-          </p>
+        <div
+          ref={droppable.setNodeRef}
+          className={cn(
+            "bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
+            droppable.isOver ? "ring-2 ring-purple-200" : ""
+          )}
+        >
+          {droppable.isOver ? (
+            <div className="p-4 w-full">
+              <div className="h-[120px] rounded-md bg-purple-200"></div>
+            </div>
+          ) : (
+            <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+              Drop here
+            </p>
+          )}
         </div>
       </div>
       <DesignerSidebar />
--- components/form-builder/elements.tsx
+++ components/form-builder/elements.tsx
@@ -5,7 +5,7 @@ export type ElementsType = "TextField"
 export type FormElement = {
   type: ElementsType
 
-  construct: (id: string) => FormElementIstance
+  construct: (id: string) => FormElementInstance
 
   designerButtonElement: {
     icon: React.ElementType
@@ -17,7 +17,7 @@ export type FormElement = {
   propertiesComponent: React.FC
 }
 
-export type FormElementIstance = {
+export type FormElementInstance = {
   id: string
   type: ElementsType
   extraAttributes?: Record<string, any>
