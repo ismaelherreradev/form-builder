04c57d3fce03174bd7890df76084a338321178cb
ismaelherreradev <ismaelherrera.dr@outlook.com>
2024-08-12 04:05:40 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,11 @@ const config = {
       },
     ],
   },
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default config
